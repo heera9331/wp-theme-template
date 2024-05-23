3273e4bac907a31bf8fcb4f61dc9a0ceb0c57651
heera9331 <heera9331@gmail.com>
2024-05-23 11:12:01 +0530

    404 added

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @package wp-theme-template
+ * 
+ */
+?>
+
+<?php get_header(); ?>
+
+
+<div class="container">
+    <h2>404 Error</h2>
+    <a href="/" class="btn btb-primary">Go to Home</a>
+</div>
+
+<?php get_footer(); ?>
